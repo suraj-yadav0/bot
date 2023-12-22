82d60a15864d6f92bbcf88375d1e1e0dfd6d8b33
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2023-12-22 19:09:58 +0530

    HOme Screen Ui is completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,23 +17,90 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Text(
               "AI",
-              style: TextStyle(color: Colors.amber,fontWeight: FontWeight.bold),
+              style:
+                  TextStyle(color: Colors.amber, fontWeight: FontWeight.bold),
             ),
             Text(
               "Psycholigist",
-              style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
             )
           ],
         ),
-        
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.notifications_active),),
+                 const SizedBox(width: 10,),
+                 CircleAvatar(child: Image.asset("assets/profile.jpg",fit: BoxFit.cover,scale: 1,),) 
+            ],
+          )
+        ],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            child: Center(
-            child: Image.asset("assets/g12.png",scale: 3,),
-          ),)
+          const SizedBox(
+            height: 75,
+          ),
+          Center(
+            child: Image.asset(
+              "assets/g12.png",
+              scale: 3,
+            ),
+          ),
+          const Center(
+            child: Text(
+              "Hi, John",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Center(
+            child: Text(
+              "I'm your AI Psychologist",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                  fontWeight: FontWeight.w500),
+            ),
+          ),
+          const Center(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                "Imagine having a supportive, non-judgmental therapist available 24/7, right on your phone."
+                " That's the AI Psychologist app.",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Container(
+              height: 50,
+              width: 200,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.blue),
+              child: const Center(
+                child: Text(
+                  "Start Chat",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
