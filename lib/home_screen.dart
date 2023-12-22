9bc66080ec40041ae2b7d15099a2cb558dced211
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2023-12-22 19:09:41 +0530

    Second Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,12 +11,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Color.fromARGB(255, 7, 76, 103),
-      appBar: AppBar(actions: []),
-      body: Column(children: [
+      // backgroundColor: Color.fromARGB(255, 1, 66, 92),
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Text(
+              "AI",
+              style: TextStyle(color: Colors.amber,fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Psycholigist",
+              style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
         
-        Center(child: Image.asset("assets/g10.png"),)
-      ],),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Center(
+            child: Image.asset("assets/g12.png",scale: 3,),
+          ),)
+        ],
+      ),
     );
   }
 }
